f64076b53938fcd0fcd254407a98ec3c4239a3c2
TienDat57 <hackertrick1135@gmail.com>
2021-06-26 08:37:35 +0700

    selection

--- 20127458.cpp
+++ 20127458.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout <<"hello dat dep trai";
+int main(){
+    int n;
+    
+    cout << "hello";
     
     system("pause");
     return 0;
